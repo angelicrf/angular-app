57727261f038520ad6f6821e1b731a7c6602155b
Your Name <angelicrf@gmail.com>
2019-12-20 14:24:37 -0800

    WformAdded

---
+++ angelique-dashboard/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {environment} from "../environments/environment";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angelique-dashboard/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAagMHcdNFK3rLghkbfk1tv-eXRx255OKs",
+    authDomain: "data-dashboard-213fc.firebaseapp.com",
+    databaseURL: "https://data-dashboard-213fc.firebaseio.com",
+    projectId: "data-dashboard-213fc",
+    storageBucket: "data-dashboard-213fc.appspot.com",
+    messagingSenderId: "310462270011",
+    appId: "1:310462270011:web:2fe8205da17feccc95aa1e",
+    measurementId: "G-BY2NQJYKP9"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
