dea52b41d947c13e92f18ffda413eb255f64664d
Your Name <angelicrf@gmail.com>
2019-12-22 11:45:02 -0800

    WTryPL

--- angelique-dashboard/src/app/login/login.component.ts
+++ angelique-dashboard/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/auth";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loading=false;
+  constructor(private afAuth: AngularFireAuth) {
+
+  }
 
   ngOnInit() {
   }
+  async onSubmit(form: NgForm){
+    this.loading =true;
+    const {email, password, firstName, lastName} = form.value;
+    console.log("Form values are:", form.value);
 
+    try{
+      const resp = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+      console.log("the response is: ", resp);
+      await resp.user.updateProfile({displayName :`${firstName} ${lastName}`});
+      form.reset();
+    }catch (e) {
+      console.log(e.message);
+    }
+    this.loading=false;
+  }
 }
