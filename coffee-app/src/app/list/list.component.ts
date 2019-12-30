277a527fb6b96481fefdc03bdadb1af235e68d26
Your Name <angelicrf@gmail.com>
2019-12-29 16:36:56 -0800

    WMap-Share

--- coffee-app/src/app/list/list.component.ts
+++ coffee-app/src/app/list/list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataService} from '../data.service';
 import {Coffee} from '../../Logic/Coffee';
 import {Router} from '@angular/router';
+import {GeolocationService} from '../geolocation.service';
 
 @Component({
   selector: 'app-list',
@@ -13,9 +14,30 @@ export class ListComponent implements OnInit {
   list: [Coffee];
 
   constructor(private data: DataService,
-              private router: Router) { }
+              private router: Router,
+              private geoLocation: GeolocationService) { }
   goDetails(coffee: Coffee) {
    this.router.navigate(['/coffee', coffee._id]);
+  }
+  goMap(coffee: Coffee) {
+    location.href = this.geoLocation.getMapLink(coffee.location);
+  }
+  share(coffee: Coffee) {
+    const shareText = `I had this coffee at ${coffee.place} and for me
+         it is a ${coffee.rating} star coffee.`;
+    const newVariable = (window.navigator as any);
+
+    if ('share' in navigator) {
+       newVariable.share({
+         title: coffee.name,
+         text: shareText,
+         url: window.location.href
+       })
+       .then( () => console.log('shared'))
+         .catch( () => console.log(('Error Sharing.')));
+     } else {
+       location.href = `whatsapp://send?text=${encodeURIComponent(shareText)}`;
+     }
   }
   ngOnInit() {
 
