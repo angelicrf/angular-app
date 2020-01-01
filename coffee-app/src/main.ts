f9e58f32c10d9e3ceccb6b6f63f9896abb05acbf
Your Name <angelicrf@gmail.com>
2019-12-31 20:05:52 -0800

    WService-Worker-try

--- coffee-app/src/main.ts
+++ coffee-app/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import {enableProdMode} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,4 +8,12 @@ if (environment.production) {
   enableProdMode();
 }
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('ngsw-worker.js')
+        .then(r => console.log('the service worker is: ', r)).catch(err => {
+          console.log('the error for the service worker is:', err);
+      });
+    }
+  })
   .catch(err => console.error(err));
