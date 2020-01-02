1803a2bb2a5d9b27d08578999cb5bd9cb7590065
Your Name <angelicrf@gmail.com>
2020-01-02 15:19:11 -0800

    WStartJS

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,7 +11,7 @@ export class DataService {
 
   constructor(private http: HttpClient ) { }
 
-  public endpoint = 'http://young-tundra-14994.herokuapp:30025';
+  public endpoint = 'http://localhost:30025';
   get(coffeeId: string, callback) {
     this.http.get(`${this.endpoint}/coffees/${coffeeId}`)
       .subscribe(response => {
