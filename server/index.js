2cee887408f0f693f67ad6619bdbc4bcfcafb7e6
Your Name <angelicrf@gmail.com>
2020-01-03 11:48:27 -0800

    WPostFetch

--- server/index.js
+++ server/index.js
@@ -13,7 +13,17 @@ restApi.addDatastore('coffees',dataStore);
 app.use(cors());
 const server_port = process.env.SERVER_PORT;
 app.use('/', restApi);
-
+let data= "somerandom string";
+fetch('http://localhost:30026/coffees', {
+  method: 'post',
+  body: JSON.stringify({
+    name: data,// this is posted on another server
+  })
+}).then(function (res) {
+  return res.text();
+}).then((body)=> {
+  console.log(body)// body can be used to get data from another server
+});
 app.listen(server_port);
 console.log(`The server is running on port :${server_port}`);
 
