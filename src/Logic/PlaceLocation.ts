3a56a1b4f210ac4f1adb54fc674a9ad10bc3cba6
Your Name <angelicrf@gmail.com>
2020-01-08 13:21:23 -0800

    SandBox

--- src/Logic/PlaceLocation.ts
+++
@@ -1,8 +0,0 @@
-export class PlaceLocation {
-
- constructor(public address: string = '',
-             public city: string = '',
-             public latitude: number = null,
-             public longitude: number = null) {
- }
-}
