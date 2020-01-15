f79bf17e610b1cfe4c5301d1d0a619fb9c94d95d
Your Name <angelicrf@gmail.com>
2020-01-15 12:16:06 -0800

    WSStarts

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/valid-app'));
 
-app.listen(process.env.PORT || 8080);
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname+`/dist/valid-app/index.html`));
+});
+const port  = process.env.PORT;
+app.listen(port || 8080);
+console.log(`The server is running on port: ${port}`);
