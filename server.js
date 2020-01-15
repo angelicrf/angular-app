4bc6bdeb3e9c3acacaf184e3e87561b4a60e4a3a
Your Name <angelicrf@gmail.com>
2020-01-15 13:26:17 -0800

    WDistec5

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname + '/dist/valid-app')));
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/valid-app/index.html'));
 });
 const port  = process.env.PORT;
