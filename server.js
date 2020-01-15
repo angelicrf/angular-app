1015d19f74047d97592a2aa4f650a9d23c47197d
Your Name <angelicrf@gmail.com>
2020-01-15 12:50:13 -0800

    WDist

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/valid-app'));
+app.use(express.static(path.join(__dirname + '/dist/valid-app')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname+`/dist/valid-app/index.html`));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/valid-app/index.html'));
 });
 const port  = process.env.PORT;
 app.listen(port || 8080);
