6076ef7a4cc45f0b3638fcaed3e3168db2d35e92
Your Name <angelicrf@gmail.com>
2020-01-17 10:05:12 -0800

    WClients

---
+++ clpanel/src/app/services/client.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import {Observable} from 'rxjs';
+import {Client} from '../../models/Client';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  client: Observable<Client>;
+  clients: Observable<Client[]>;
+  clientsCollection: AngularFirestoreCollection<Client>;
+  clientDoc: AngularFirestoreDocument<Client>;
+
+  constructor(private fs: AngularFirestore) {
+    this.clientsCollection = this.fs.collection('clients', ref => ref.orderBy('lastName', 'asc'));
+  }
+  getClient(): Observable<Client[]> {
+    this.clients = this.clientsCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Client;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+    return this.clients;
+  }
+}
--- clpanel/src/environments/environment.ts
+++ clpanel/src/environments/environment.ts
@@ -1,16 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyChru824an4EBi7hDWvhWs1q_yXtB_bgcs',
+    authDomain: 'projectpanel-4472b.firebaseapp.com',
+    databaseURL: 'https://projectpanel-4472b.firebaseio.com',
+    projectId: 'projectpanel-4472b',
+    storageBucket: 'projectpanel-4472b.appspot.com',
+    messagingSenderId: '729472950210',
+    appId: '1:729472950210:web:49f1d18f0b15c78f2d805b',
+    measurementId: 'G-VV50RZH9X6'
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
